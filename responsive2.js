1577913c1ffb338ec285677b97f5ee518735f4fc
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-07-05 22:20:05 +0300

    upgrade with js

--- responsive2.js
+++ responsive2.js
@@ -4,12 +4,12 @@ const seeMobileTop = document.getElementById('mobile-top');
 seeMobileMenu.addEventListener('click', function() {
     console.log('click');
 
-    if(seeMobileTop.style.display === "") {
+    if(seeMobileTop.style.display === "none") {
         seeMobileTop.style.display = "block";
         console.log('mobile nav bar');
         
     } else {
-        seeMobileTop.style.display = "";
+        seeMobileTop.style.display = "none";
         console.log('mobile nav bar not');
     }
 });
