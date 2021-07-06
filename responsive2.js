3034d7f27a0260bcf8d4cbfa601d1c4f84b2d1da
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-07-06 20:02:22 +0300

    update with js

--- responsive2.js
+++ responsive2.js
@@ -1,5 +1,6 @@
 const seeMobileMenu = document.getElementById("menu-bars");
 const seeMobileTop = document.getElementById('mobile-top');
+const seeCloseBtn = document.getElementById('close-btn')
 
 seeMobileMenu.addEventListener('click', function() {
     console.log('click');
@@ -8,8 +9,18 @@ seeMobileMenu.addEventListener('click', function() {
         seeMobileTop.style.display = "block";
         console.log('mobile nav bar');
         
-    } else {
-        seeMobileTop.style.display = "none";
-        console.log('mobile nav bar not');
+     } 
+     //else {
+    //     seeMobileTop.style.display = "none";
+    //     console.log('mobile nav bar not');
+    // }
+});
+
+seeCloseBtn.addEventListener('click', function () {
+    console.log('click');
+
+    if(seeMobileTop.style.display !== "none") {
+        seeMobileTop.style.display = "none"
+        console.log('closed');
     }
 });
